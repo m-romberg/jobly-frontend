681e89ad5af6ffa37d139c4cda3779a6ada184d5
Chris Lupton <42554017+clupt@users.noreply.github.com>
2023-04-26 16:53:04 -0500

    updated company card to return company detail page when clicked

--- src/CompanyCard.js
+++ src/CompanyCard.js
@@ -1,3 +1,5 @@
+import { Navigate } from "react-router-dom";
+import { useState } from "react";
 /**
 * CompanyCard:
 *     simple visual element of Company
@@ -11,14 +13,27 @@
 */
 
 function CompanyCard({ companyData }) {
+  console.log("compd=", companyData);
+
+  const [isClicked, setIsClicked] = useState(false);
+
+  if(isClicked === true) {
+    return <Navigate to={`/companies/${companyData.handle}`}/>
+  }
+
+  function activateIsClicked() {
+    setIsClicked(true);
+  }
 
   return (
     <div className="CompanyCard">
-      <h4>{companyData.name}</h4>
-      {companyData.logoUrl
-        &&
-        <img src={companyData.logoUrl} alt={companyData.name}></img>}
-      <p>{companyData.description}</p>
+        <button className="CompanyCard-button" onClick={activateIsClicked}>
+        <h4>{companyData.name}</h4>
+        {companyData.logoUrl
+          &&
+          <img src={companyData.logoUrl} alt={companyData.name}></img>}
+        <p>{companyData.description}</p>
+        </button>
     </div>
   );
 }
