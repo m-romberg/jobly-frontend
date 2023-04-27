2fe1ec949f56d5974f9a2269bf2acfe19ec41b20
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-04-27 15:13:57 -0700

    create loginform component

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -14,10 +14,60 @@ import Alert from "./Alert";
  * RoutesList --> LoginForm --> Alert
  */
 
-function LoginForm() {
+function LoginForm({ handleLogin }) {
   console.log("LoginForm ran");
+  const initialState = {
+    username: "",
+    password: "",
+  };
+
+  const [formData, setFormData] = useState(initialState);
+  console.log("formData", formData);
+
+  /** Update form input. */
+  function handleChange(evt) {
+    const input = evt.target;
+    setFormData(formData => ({
+      ...formData,
+      [input.name]: input.value,
+    }));
+  }
+
+  /** Call parent function and clear form. */
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    handleLogin(formData);
+  }
   return (
-    <div className="LoginForm">Login form!</div>
+    <div className="LoginForm">
+      <form className="LoginForm-form" onSubmit={handleSubmit}>
+        <div className="LoginForm-username">
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            name="username"
+            className="form-control"
+            placeholder="username"
+            onChange={handleChange}
+            value={formData.username}
+          />
+        </div>
+        <div className="LoginForm-password">
+          <label htmlFor="password">Password</label>
+          <input
+            id="password"
+            type="password"
+            name="password"
+            className="form-control"
+            placeholder="password"
+            onChange={handleChange}
+            value={formData.password}
+          />
+        </div>
+        {/**alertMessages && alertMessages.map(m => <Alert message={m} />)*/}
+        <button className="LoginForm-btn">Submit</button>
+      </form>
+    </div>
   );
 }
 
