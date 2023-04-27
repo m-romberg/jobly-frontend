e9c0875b22e16cc56b6571a8f66d4253e6a2d8ee
Chris Lupton <42554017+clupt@users.noreply.github.com>
2023-04-27 15:53:06 -0500

    add routes for signup, login, and profile

--- src/RoutesList.js
+++ src/RoutesList.js
@@ -13,15 +13,29 @@ import Homepage from "./Homepage";
  */
 
 function RoutesList() {
-  return (
-    <Routes>
+  const username = undefined;
+
+  const loggedInRoutes =
+    <div className="Routes-loggedIn">
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/companies" element={<CompanyList />} />
+        <Route path="/companies/:handle" element={<CompanyDetail />} />
+        <Route path="/jobs" element={<JobList />} />
+        <Route path="/profile" element={<ProfileForm />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </div>;
+
+  const loggedOutRoutes =
+    <div className="Routes-loggedOut">
       <Route path="/" element={<Homepage />} />
-      <Route path="/companies" element={<CompanyList />} />
-      <Route path="/jobs" element={<JobList />} />
-      <Route path="/companies/:handle" element={<CompanyDetail />} />
+      <Route path="/login" element={<LoginForm />} />
+      <Route path="/signup" element={<SignupForm />} />
       <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
-  );
+    </div>;
+
+  return username ? loggedInRoutes : loggedOutRoutes;
 }
 
 export default RoutesList;
