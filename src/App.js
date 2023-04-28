308040f14d4254116a41913b8933d21a47bc6e00
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-04-28 12:05:55 -0700

    install react-jwt and change handle of curruserdata

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import RoutesList from './RoutesList';
 import JoblyApi from './api';
 import { useState, useEffect } from 'react';
 import userContext from "./userContext";
+import { decodeToken } from "react-jwt";
 
 /**
  *
@@ -13,7 +14,7 @@ import userContext from "./userContext";
  *  Handles login and sign up validation
  *
  * state:
- *    token - sets user's JWT
+ *    token - null or user's JWT
  *    currUserData - {username,
  *                    firstName,
  *                    lastName,
@@ -37,23 +38,23 @@ function App() {
     isAdmin: null,
     applications: null
   };
-  //TODO: set token to null --update docstring (str or null)
-  const [token, setToken] = useState("");
+  const [token, setToken] = useState(null);
   const [currUserData, setCurrUserData] = useState(initialState);
   const [errorMessages, setErrorMessages] = useState(null);
 
   console.log("App state=", "token=", token, "currUserData", currUserData);
   console.log("error msgs in app", errorMessages);
-  //TODO: grab username from payload of token on line 53 instead
+
   /** Gets user data on initial render and token change */
   useEffect(function fetchUserOnTokenChange() {
     console.log("inside fetchUserOnTokenChange");
     async function getUser() {
       console.log("inside getUser");
-      if (currUserData.username !== null) {
+      if (token !== null) {
+        const username = decodeToken(token).username;
+        console.log("jwt decode username=", username);
         try {
-          console.log("currUserData", currUserData);
-          const userResult = await JoblyApi.getUser(currUserData.username, token);
+          const userResult = await JoblyApi.getUser(username, token);
           console.log("userResult", userResult);
           setCurrUserData(userResult);
         } catch (error) {
@@ -66,7 +67,7 @@ function App() {
   }, [token]);
 
   /** Logs in a user or sets error messages */
-  //TODO: 77-80 will change after token change
+
   //TODO: update error message handling to empty them out somewhere
   async function login(loginData) {
     console.log("inside login");
@@ -75,10 +76,6 @@ function App() {
       const token = await JoblyApi.loginUser({ username, password });
       setToken(token);
       JoblyApi.token = token;
-      setCurrUserData(curr => {
-        curr['username'] = username;
-        return { ...curr };
-      });
     } catch (error) {
       console.log("error in login", error);
       setErrorMessages(error);
@@ -86,7 +83,6 @@ function App() {
   }
 
   /** Signs up a user or sets error messages */
-  //TODO: dont need 99-101 after token change above
   async function signup(signupData) {
     console.log("inside signup");
     const { username, password, firstName, lastName, email } = signupData;
@@ -97,10 +93,6 @@ function App() {
         );
       setToken(token);
       JoblyApi.token = token;
-      setCurrUserData(curr => {
-        curr['username'] = username;
-        return { ...curr };
-      });
     } catch (error) {
       console.log("error in signup", error);
       setErrorMessages(error);
@@ -108,10 +100,9 @@ function App() {
   }
 
   /** Logs out a user and sets token to empty string */
-  //TODO: change token reset to null here as well
   function logout() {
     console.log("inside logout");
-    setToken("");
+    setToken(null);
     setCurrUserData(initialState);
   }
 
