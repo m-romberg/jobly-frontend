cceba67884f4b2723a716e0f0d8ba09ffdb3c517
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-06-27 17:52:51 -0700

    fix homepage css

--- src/Homepage.js
+++ src/Homepage.js
@@ -17,44 +17,31 @@ function Homepage() {
   console.log("username in navigation=", username);
 
   const loggedInHomepageContent =
-    <Container className="Homepage-loggedInHomepageContent">
-      <Row><Col>
+    <div className="Homepage-loggedInHomepageContent">
         <p>Welcome back, {username}!</p>
-      </Col></Row>
-    </Container>;
+    </div>;
   //TODO: updsate classnames w component names first
   const loggedOutHomepageContent =
-    <Container className="Homepage-loggedOutHomepageContent">
-      <Row>
-        <Col>
+    <div className="Homepage-loggedOutHomepageContent">
+
           <Link to="/login">
             <button className="Homepage-login-btn">Log in</button>
           </Link>
-        </Col>
-        <Col>
+
           <Link to="/signup">
             <button className="Homepage-signup-btn">Sign up</button>
           </Link>
-        </Col>
-      </Row>
-    </Container>;
+
+    </div>;
 
   return (
-    <Container className="Homepage">
-      <Row>
-        <Col>
+    <div className="Homepage">
           <h1 className="Homepage-title">Jobly</h1>
-        </Col>
-      </Row>
-      <Row>
-        <Col>
           <h3 className="Homepage-description">
             All the jobs in one, convenient place.
           </h3>
-        </Col>
-      </Row>
       {username ? loggedInHomepageContent : loggedOutHomepageContent}
-    </Container>
+    </div>
 
   );
 }
